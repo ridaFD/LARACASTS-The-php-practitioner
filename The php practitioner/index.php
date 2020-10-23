de5ad80d41db5b12f2599673c2680b833f76351f
Rida fd <ridafakherlden@gmail.com>
2020-10-24 00:53:38 +0300

    Episode 4:PHP and HTML

--- The php practitioner/index.php
+++ The php practitioner/index.php
@@ -1,8 +1,28 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Php learning</title>
 
-$name = 'Jane Doe';
-$greeting = 'Hello';
+    <style>
+        header {
+            background-color: gray;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
 
-echo $greeting . ', ' . $name;
-echo "$greeting, $name";
-echo "{$greeting}, {$name}";
+</head>
+<body>
+
+    <header>
+        <h1>
+            <?php echo 'Hello, ' . htmlspecialchars($_GET['name']) ?>
+        </h1>
+    </header>
+
+</body>
+</html>
